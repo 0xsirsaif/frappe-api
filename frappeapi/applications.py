e1fd45cbcdd795b74d125ce1771ce2f8212ac3a0
saif <sirsaif99@gmail.com>
2024-10-05 12:38:56 +0300

    ref: Complete the code flow without any parsing, validation, and serialixation. Time time test #2

--- frappeapi/applications.py
+++ frappeapi/applications.py
@@ -1,13 +1,8 @@
-import json
 from enum import Enum
 from typing import Any, Dict, List, Optional, Union
 
-from openapi_pydantic_v2 import Info, OpenAPI, Operation, PathItem, Response, Server
-from pydantic import BaseModel
-from typing_extensions import Annotated, Doc
-from werkzeug.wrappers import Response as WerkzeugResponse
-
 from frappeapi.routing import APIRouter
+from frappeapi.utils import Default
 
 
 class FrappeAPI:
@@ -29,7 +24,7 @@ class FrappeAPI:
 	def get(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -48,7 +43,7 @@ class FrappeAPI:
 	def post(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -67,7 +62,7 @@ class FrappeAPI:
 	def put(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -86,7 +81,7 @@ class FrappeAPI:
 	def delete(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -105,7 +100,7 @@ class FrappeAPI:
 	def patch(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -143,7 +138,7 @@ class FrappeAPI:
 	def head(
 		self,
 		*,
-		response_model: Any = None,
+		response_model: Any = Default(None),
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
 		tags: Optional[List[Union[str, Enum]]] = None,
@@ -158,223 +153,3 @@ class FrappeAPI:
 			summary=summary,
 			include_in_schema=include_in_schema,
 		)
-
-	def __init__(
-		self,
-		title: Annotated[
-			str,
-			Doc(
-				"""
-                    The title of the API.
-                    It will be added to the generated OpenAPI (e.g., visible at `/docs`).
-                    """
-			),
-		] = "FrappeAPI",
-		summary: Annotated[
-			Optional[str],
-			Doc(
-				"""
-                    A short summary of the API.
-                    It will be added to the generated OpenAPI (e.g., visible at `/docs`).
-                    """
-			),
-		] = None,
-		description: Annotated[
-			str,
-			Doc(
-				"""
-                    A description of the API. Supports Markdown (using [CommonMark syntax](https://commonmark.org/)).
-                    It will be added to the generated OpenAPI (e.g., visible at `/docs`).
-                    """
-			),
-		] = "",
-		version: Annotated[
-			str,
-			Doc(
-				"""
-                    The version of the API.
-                    Note: This is the version of your application, not the OpenAPI specification nor FastAPI.
-                    It will be added to the generated OpenAPI (e.g., visible at `/docs`).
-                    """
-			),
-		] = "0.1.0",
-		servers: Annotated[
-			Optional[List[Dict[str, Union[str, Any]]]],
-			Doc(
-				"""
-                A `list` of `dict`s with connectivity information to a target server.
-
-                You would use it, for example, if your application is served from
-                different domains and you want to use the same Swagger UI in the
-                browser to interact with each of them (instead of having multiple
-                browser tabs open). Or if you want to leave fixed the possible URLs.
-
-                If the servers `list` is not provided, or is an empty `list`, the
-                default value would be a `dict` with a `url` value of `/`.
-
-                Each item in the `list` is a `dict` containing:
-
-                * `url`: A URL to the target host. This URL supports Server Variables
-                and MAY be relative, to indicate that the host location is relative
-                to the location where the OpenAPI document is being served. Variable
-                substitutions will be made when a variable is named in `{`brackets`}`.
-                * `description`: An optional string describing the host designated by
-                the URL. [CommonMark syntax](https://commonmark.org/) MAY be used for
-                rich text representation.
-                * `variables`: A `dict` between a variable name and its value. The value
-                    is used for substitution in the server's URL template.
-
-                ```python
-                from frappeapi import FrappeAPI
-
-                app = FrappeAPI(
-                    servers=[
-                        {"url": "https://stag.example.com", "description": "Staging environment"},
-                        {"url": "https://prod.example.com", "description": "Production environment"},
-                    ]
-                )
-                ```
-                """
-			),
-		] = None,
-	):
-		self.title = title
-		self.summary = summary
-		self.description = description
-		self.version = version
-		self.routes: List[Dict[str, Any]] = []
-		self.openapi_schema: Optional[Dict[str, Any]] = None
-		self.servers = servers or [{"url": "/"}]
-		self.router = APIRouter(
-			prefix="/api/method",
-			default_response_class=WerkzeugResponse,
-		)
-
-	def openapi_json(self):
-		openapi_schema = self._openapi().model_dump(by_alias=True, exclude_none=True)
-		return json.dumps(openapi_schema, indent=2)
-
-	def _openapi(self):
-		if not self.openapi_schema:
-			paths = {}
-			for item in self.routes:
-				paths[item["path"]] = item["item"]
-
-			openapi_model = OpenAPI(
-				openapi="3.0.0",
-				info=Info(title=self.title, version=self.version, description=self.description),
-				paths=paths,
-				servers=[Server(**server) for server in self.servers],
-			)
-			return openapi_model
-
-		return self.openapi_schema
-
-	def add_route(
-		self,
-		path: str,
-		method: str,
-		response_model: BaseModel,
-		parameters: List[Dict[str, Any]] | None = None,
-	):
-		# Correctly handle adding a new path or appending to an existing one
-		path_item = next((item for item in self.routes if item["path"] == path), None)
-		if not path_item:
-			path_item = {"path": path, "item": PathItem()}
-			self.routes.append(path_item)
-
-		# Dynamically set the operation based on the HTTP method
-		operation = Operation(
-			parameters=parameters,
-			responses={
-				"200": Response(
-					description="Successful response",
-					content={"application/json": {"schema": response_model.model_json_schema()}},
-				)
-			},
-		)
-		setattr(path_item["item"], method.lower(), operation)
-
-	def get(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.get(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def post(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Annotated[Optional[int], Doc("The status code to return")] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.post(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def put(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.put(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def delete(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.delete(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def patch(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.patch(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def options(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.options(response_model, status_code, description, tags, summary, include_in_schema)
-
-	def head(
-		self,
-		*,
-		response_model: Any = None,
-		status_code: Optional[int] = None,
-		description: Optional[str] = None,
-		tags: Optional[List[Union[str, Enum]]] = None,
-		summary: Optional[str] = None,
-		include_in_schema: bool = True,
-	):
-		self.router.head(response_model, status_code, description, tags, summary, include_in_schema)
--- frappeapi/routing.py
+++ frappeapi/routing.py
@@ -1,8 +1,13 @@
-from typing import Any, Callable, Enum, List, Optional, Type, Union
+import inspect
+import json
+from enum import Enum
+from typing import Any, Callable, List, Optional, Set, Type, Union
 
+from frappe import whitelist
 from werkzeug.wrappers import Response as WerkzeugResponse
 
 from .utils import (
+	Default,
 	extract_endpoint_relative_path,
 )
 
@@ -13,7 +18,7 @@ class APIRoute:
 		path: str,
 		func: Callable,
 		*,
-		methods: List[str],
+		methods: Optional[Union[Set[str], List[str]]] = None,
 		response_model: Any = None,
 		status_code: Optional[int] = None,
 		description: Optional[str] = None,
@@ -23,7 +28,11 @@ class APIRoute:
 	):
 		self.path = path
 		self.func = func
-		self.methods = methods
+
+		if methods is None:
+			methods = ["GET"]
+		self.methods: Set[str] = {method.upper() for method in methods}
+
 		self.response_model = response_model
 		self.status_code = status_code
 		self.description = description
@@ -31,21 +40,44 @@ class APIRoute:
 		self.summary = summary
 		self.include_in_schema = include_in_schema
 
+	def handle_request(self, *args, **kwargs):
+		try:
+			# Remove known Frappe-specific arguments
+			kwargs.pop("cmd", None)
+
+			# Get the function's signature
+			sig = inspect.signature(self.func)
+
+			# Filter arguments to only those the function expects
+			bound_args = sig.bind(*args, **kwargs)
+			bound_args.apply_defaults()
+
+			# Call the function with the filtered arguments
+			result = self.func(*bound_args.args, **bound_args.kwargs)
+
+			# Convert the result to JSON and return a response
+			response_content = json.dumps(result)
+			return WerkzeugResponse(
+				response_content, status=self.status_code or 200, mimetype="application/json"
+			)
+		except Exception as exc:
+			# Error handling
+			error_response = {"detail": str(exc)}
+			response_body = json.dumps(error_response)
+			return WerkzeugResponse(response_body, status=500, mimetype="application/json")
+
 
 class APIRouter:
 	def __init__(
 		self,
-		prefix: str,
-		default_response_class: Type[WerkzeugResponse] = WerkzeugResponse,
+		prefix: str = "/api/method",
+		default_response_class: Type[WerkzeugResponse] = Default(WerkzeugResponse),
 	):
 		if prefix:
-			assert prefix.startswith("/"), "A path prefix must start with '/'"  # noqa: S101
-			assert not prefix.endswith(  # noqa: S101
+			assert prefix.startswith("/"), "A path prefix must start with '/'"
+			assert not prefix.endswith(
 				"/"
 			), "A path prefix must not end with '/', as the routes will start with '/'"
-		else:
-			prefix = "/api/method"
-
 		self.prefix = prefix
 		self.default_response_class = default_response_class
 		self.routes: List[APIRoute] = []
@@ -60,21 +92,22 @@ class APIRouter:
 		tags: Optional[List[Union[str, Enum]]] = None,
 		summary: Optional[str] = None,
 		include_in_schema: bool = True,
-		methods: Optional[List[str]] = None,
+		methods: Optional[Union[Set[str], List[str]]] = None,
 	):
 		path = self.prefix + extract_endpoint_relative_path(func) + "." + func.__name__
 		route = APIRoute(
 			path,
 			func,
-			methods,
-			response_model,
-			status_code,
-			description,
-			tags,
-			summary,
-			include_in_schema,
+			methods=methods,
+			response_model=response_model,
+			status_code=status_code,
+			description=description,
+			tags=tags,
+			summary=summary,
+			include_in_schema=include_in_schema,
 		)
 		self.routes.append(route)
+		return route
 
 	def api_route(
 		self,
@@ -87,17 +120,22 @@ class APIRouter:
 		include_in_schema: bool = True,
 		methods: Optional[List[str]] = None,
 	):
-		def decorator(func):
-			self.add_api_route(
-				func,
-				response_model=response_model,
-				status_code=status_code,
-				description=description,
-				tags=tags,
-				summary=summary,
-				include_in_schema=include_in_schema,
-				methods=methods,
-			)
+		def decorator(func: Callable):
+			@whitelist(methods=methods)
+			def wrapper(*args, **kwargs):
+				route = self.add_api_route(
+					func,
+					response_model=response_model,
+					status_code=status_code,
+					description=description,
+					tags=tags,
+					summary=summary,
+					include_in_schema=include_in_schema,
+					methods=methods,
+				)
+				return route.handle_request(*args, **kwargs)
+
+			return wrapper
 
 		return decorator
 
@@ -121,20 +159,74 @@ class APIRouter:
 			include_in_schema=include_in_schema,
 		)
 
-	def post(self):
+	def post(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
 
-	def put(self):
+	def put(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
 
-	def delete(self):
+	def delete(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
 
-	def patch(self):
+	def patch(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
 
-	def options(self):
+	def options(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
 
-	def head(self):
+	def head(
+		self,
+		*,
+		response_model: Any = None,
+		status_code: Optional[int] = None,
+		description: Optional[str] = None,
+		tags: Optional[List[Union[str, Enum]]] = None,
+		summary: Optional[str] = None,
+		include_in_schema: bool = True,
+	):
 		pass
