560a01bb8a8e033f10ff9f7ca968d849130fc351
saif <sirsaif99@gmail.com>
2024-10-08 17:59:08 +0300

    Restructure files #2

--- frappeapi/utils.py
+++ frappeapi/utils.py
@@ -1,12 +1,6 @@
 import inspect
-import json
 import os
-import typing
-from inspect import Parameter, signature
 from typing import Any
-
-from pydantic import ValidationError, create_model
-from werkzeug.wrappers import Response
 
 
 class DefaultPlaceholder:
@@ -35,61 +29,3 @@ def extract_endpoint_relative_path(func):
 		return ".".join(sub_path_parts)
 	except ValueError:
 		return None
-
-
-def create_openapi_param_dict(func) -> list[dict]:
-	sig = inspect.signature(func)
-	type_hints = typing.get_type_hints(func)
-	openapi_params = []
-	for name, param in sig.parameters.items():
-		param_type = type_hints.get(name, str)  # Default to string if type hint missing
-		openapi_type = map_type_to_openapi(param_type)
-		param_dict = {
-			"name": name,
-			"in": "query",
-			"required": param.default is inspect.Parameter.empty,  # Required if default is not set
-			"schema": {"type": openapi_type},
-		}
-		openapi_params.append(param_dict)
-
-	return openapi_params
-
-
-def map_type_to_openapi(py_type) -> str:
-	mapping = {int: "integer", float: "number", str: "string", bool: "boolean"}
-	return mapping.get(py_type, "string")
-
-
-def create_validator_model(func):
-	"""
-	Create a Pydantic model dynamically based on function's annotations and defaults.
-	"""
-	annotations = typing.get_type_hints(func)
-	fields = {
-		name: (annotations[name], param.default if param.default is not Parameter.empty else ...)
-		for name, param in signature(func).parameters.items()
-		if name != "return"
-	}
-
-	return create_model(f"{func.__name__}_ValidationModel", **fields)
-
-
-def format_validation_error(error: ValidationError, loc: str = "query") -> Response:
-	"""
-	Format the Pydantic ValidationError to match FastAPI error response style.
-	"""
-	details = []
-	for err in error.errors():
-		details.append(
-			{
-				"type": err["type"],
-				"loc": [loc] + list(err["loc"]),
-				"msg": err["msg"],
-				"input": err.get("ctx", {}).get("input"),
-			}
-		)
-	error_response = {"detail": details}
-
-	response_body = json.dumps(error_response)
-
-	return Response(response_body, status=422, mimetype="application/json")
