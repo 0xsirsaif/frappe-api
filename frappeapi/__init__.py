cfa5d788f8f0ff0b2df6e0d7ca25f0181982b46c
saif <sirsaif99@gmail.com>
2024-10-28 23:58:33 +0300

    Support request body. File and UploadFile is not working #2

--- frappeapi/__init__.py
+++ frappeapi/__init__.py
@@ -1,7 +1,12 @@
 __version__ = "0.0.1"
 
+from fastapi.datastructures import UploadFile  # noqa: F401
 from fastapi.params import (
+	Body,  # noqa: F401
 	Depends,  # noqa: F401
+	File,  # noqa: F401
+	Form,  # noqa: F401
+	Header,  # noqa: F401
 	Query,  # noqa: F401
 )
 
