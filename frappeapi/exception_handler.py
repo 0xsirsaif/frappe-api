d9600589f10deb6b452091fd6a280c6e390223bd
saif <sirsaif99@gmail.com>
2024-11-03 22:16:15 +0200

    Return JSONResponse for default exception handlers {ResponseValidationError, HTTPException, RequestValidationError}

--- frappeapi/exception_handler.py
+++ frappeapi/exception_handler.py
@@ -1,21 +1,20 @@
-import json
-
 from fastapi.utils import is_body_allowed_for_status_code
 from werkzeug.wrappers import Request as WerkzeugRequest, Response as WerkzeugResponse
 
 from frappeapi.exceptions import HTTPException, RequestValidationError, ResponseValidationError
+from frappeapi.responses import JSONResponse
 
 
 def request_validation_exception_handler(request: WerkzeugRequest, exc: RequestValidationError) -> WerkzeugResponse:
-	return WerkzeugResponse(response=json.dumps({"detail": exc.errors()}), status=422, content_type="application/json")
+	return JSONResponse(content={"detail": exc.errors()}, status_code=422)
 
 
 def http_exception_handler(request: WerkzeugRequest, exc: HTTPException) -> WerkzeugResponse:
 	headers = getattr(exc, "headers", None)
 	if not is_body_allowed_for_status_code(exc.status_code):
-		return WerkzeugResponse(status=exc.status_code, headers=headers)
+		return JSONResponse(status_code=exc.status_code, headers=headers)
 
-	return WerkzeugResponse({"detail": exc.detail}, status=exc.status_code, headers=headers)
+	return JSONResponse(content={"detail": exc.detail}, status_code=exc.status_code, headers=headers)
 
 
 def response_validation_exception_handler(request: WerkzeugRequest, exc: ResponseValidationError) -> WerkzeugResponse:
@@ -26,5 +25,4 @@ def response_validation_exception_handler(request: WerkzeugRequest, exc: Respons
 	> not returning what it should, and it will return a server error instead of returning incorrect data.
 	> This way you and your clients can be certain that they will receive the data and the data shape expected.
 	"""
-	empty_body = b""
-	return WerkzeugResponse(response=empty_body, status=500, content_type="application/json")
+	return JSONResponse(content={"detail": exc.errors()}, status_code=500)
